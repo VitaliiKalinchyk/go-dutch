0db47a257932d76e5f5653b66ac0fd91a5cdb2a6
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-07 22:43:42 +0300

    GoDutch

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,23 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
-        //Write code here
+        Scanner input = new Scanner(System.in);
+        int price = input.nextInt();
+        if (price<0) {
+            System.out.println("Bill total amount cannot be negative");
+        }
+        else {
+            int friends = input.nextInt();
+            if (friends<1){
+                System.out.println("Number of friends cannot be negative or zero");
+            }
+            else {
+                System.out.println((int)(price * 1.1 / friends));
+            }
+        }
     }
 }
